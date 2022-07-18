fa086fc2f1e9650ff44bdf0a9d884e8b8ca229a7
alina1412 <alina1412@mail.ru>
2022-07-18 08:55:37 +0300

    db vars

--- survey_project/settings.py
+++ survey_project/settings.py
@@ -113,7 +113,13 @@ if DEBUG:
     }
 else:
     DATABASES = {
-        'default': parse_db_url()
+        # 'default': parse_db_url()
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("NAME"),
+        'USER': os.environ.get("USER"),
+        'PASSWORD': os.environ.get("PASSWORD"),
+        'HOST': os.environ.get("HOST"),
+        'PORT': os.environ.get("PORT"),
     }
 
 
